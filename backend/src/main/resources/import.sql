bb6d7065743a6b16df39d193be8ebab201fd4890
Fábio Monteiro <fabio.monteiro.21@hotmail.com>
2022-02-28 12:06:47 -0300

    Offer

--- backend/src/main/resources/import.sql
+++ backend/src/main/resources/import.sql
@@ -14,3 +14,6 @@ INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 2);;
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 3);
 
 INSERT INTO tb_course (name, img_Uri, img_Gray_Uri) VALUES ('Bootcamp HTML','https://images.rawpixel.com/image_800/czNmcy1wcml2YXRlL3Jhd3BpeGVsX2ltYWdlcy93ZWJzaXRlX2NvbnRlbnQvcGQ0My0wNjA0LTEzOC1leWUtMDJfMC5qcGc.jpg?s=bgFBZrrV3ig6CjDUEb1RwlywJENRW9oUv3Li8nrzRIw', 'https://images.rawpixel.com/image_800/czNmcy1wcml2YXRlL3Jhd3BpeGVsX2ltYWdlcy93ZWJzaXRlX2NvbnRlbnQvcGQ0My0wNjA0LTEzOC1leWUtMDJfMC5qcGc.jpg?s=bgFBZrrV3ig6CjDUEb1RwlywJENRW9oUv3Li8nrzRIw');
+
+INSERT INTO tb_offer (edition, start_Moment, end_Moment, course_id) VALUES ('1.0', TIMESTAMP WITH TIME ZONE '2020-11-20T03:00:00Z', TIMESTAMP WITH TIME ZONE '2021-11-20T03:00:00Z', '1');
+INSERT INTO tb_offer (edition, start_Moment, end_Moment, course_id) VALUES ('2.0', TIMESTAMP WITH TIME ZONE '2020-12-20T03:00:00Z', TIMESTAMP WITH TIME ZONE '2021-12-20T03:00:00Z', '1');
